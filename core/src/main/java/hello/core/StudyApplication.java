2543f9c7b31cad6eeb649eaaf416fc038d220ca3
최민석 <cms8955@naver.com>
2024-01-09 01:06:28 +0900

    inflearn: 8장 빈 생명주기 콜백

---
+++ core/src/main/java/hello/core/StudyApplication.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class CoreApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApplicationTests.class, args);
+	}
+
+}
