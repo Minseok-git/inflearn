7bae12e996e3d2db0e732f87b9ebeefeab1c918a
최민석 <cms8955@naver.com>
2024-01-09 10:47:10 +0900

    inflearn: Rename CoreApplication.java

--- core/src/main/java/hello/core/CoreApplication.java
+++ core/src/main/java/hello/core/CoreApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class CoreApplicationTests {
+class CoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoreApplicationTests.class, args);
+		SpringApplication.run(CoreApplication.class, args);
 	}
-
 }
